57c572b8789c65917c3daf256fbfaa12d0c50e8c
park <jay1415@naver.com>
2021-07-21 12:40:45 +0900

    7/21 class commit 4

--- decorators.py
+++ decorators.py
@@ -14,3 +14,26 @@ def hello_world(input_text):
 
 
 hello_world('hello world!')
+
+
+def positive_value(func):
+    def decorated(width, height):
+        if width >= 0 and height >= 0:
+            return func(width, height)
+        else:
+            print('ERROR')
+    return decorated
+
+
+@positive_value
+def triangle(width, height):
+    print(width * height * 0.5)
+
+
+@positive_value
+def square(width, height):
+    print(width * height)
+
+
+square(-5, 10)
+(triangle(10, 10))
